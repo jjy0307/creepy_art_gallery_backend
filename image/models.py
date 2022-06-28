bffb5eee733ad86f7e54da7d2cd0d44b081bdacd
soiyo <soiyosauce@gmail.com>
2022-06-28 15:06:12 +0900

    ADD : project, apps, base model

--- image/models.py
+++ image/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from user.models import CustomUser
 # Create your models here.
+class Image(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    image = models.ImageField(null=False)
+    name = models.CharField(max_length=256)
+    like = models.ArrayField(null=False)
