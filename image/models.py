d28cd6ecdf7f9780866231f2e2cb7e106d248832
soiyo <soiyosauce@gmail.com>
2022-07-01 17:30:28 +0900

    MOD image 앱models.py 내용 수정

--- image/models.py
+++ image/models.py
@@ -3,8 +3,12 @@ from user.models import User
 # Create your models here.
 
 
-class Image(models.Model):
+class ImageModel(models.Model):
+    title = models.CharField(max_length=256)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(null=False)
-    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='images/',blank=True, null=True)
     # like = models.ArrayField(null=False)
+
+    # 제목으로 파일을 구분할 수 있게
+    def __str__(self):
+        return str(self.title)
