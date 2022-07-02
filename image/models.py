23ca9869fae6bf2d51fff86036e80d76e3873f15
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-02 17:57:00 +0900

    MOD image/models.py
    UserModel -> User

--- image/models.py
+++ image/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from user.models import UserModel
+from user.models import User as UserModel
 # Create your models here.
 
 
@@ -7,7 +7,8 @@ class ImageModel(models.Model):
     title = models.CharField(max_length=256)
     # like = models.ArrayField(null=False)
     # user = models.ForeignKey(UserModel, on_delete=models.CASCADE, null=False)
-    image = models.ImageField(upload_to='images/',blank=True, null=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 # 제목으로 파일을 구분할 수 있게
+
     def __str__(self):
         return str(self.title)
