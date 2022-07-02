c43593e782533f3906f5f996c7b68644edcb03aa
soiyo <soiyosauce@gmail.com>
2022-07-03 04:50:37 +0900

    MOD image/views.py  upload 함수 : 메인페이지 사진 2장 업로드시 모델에 적용시켜 출력물 내는 함수 작성했습니다. 사이트 테스트하면서 사진이 잘 들어가는지 쉽게 확인하기 위해 현재는 메인페이지 들어가면 가장 최근에 업로드했던 사진 2장이 띄워지게 작성해놓은 상태입니다. 사진 업로드하고 업로드 버튼 누를시 출력되는 사진은 로컬에 저장되면서 사진url이 반환됩니다. (nst.py 함수화하여 출력사진url 반환되게 구현해놓았습니다.)

--- image/views.py
+++ image/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from nst import change_image
 # Create your views here.
+from .models import ImageModel
+import os
+
+def upload(request):
+    if request.method == "POST":
+        images = request.FILES.getlist("images")
+
+        for img in images:
+            ImageModel.objects.create(images=img)
+    images = ImageModel.objects.all() #input 이미지 url 불러옴
+    images_slice = ImageModel.objects.all().order_by('-timestamp')[0:2] # 가장 최근에 넣은 input 이미지 url
+    # print(images_slice)
+    input_images_list= []
+    for image_num in (0,2):
+        try:
+            images_values = ImageModel.objects.values('images').order_by('-timestamp')[image_num]['images']  
+            input_images_list.append(images_values)
+        except:
+            pass
+    print(input_images_list)
+    try:
+        result_image_path=change_image(input_images_list[0],input_images_list[1])
+        result_image_path
+        # change_image(input_images_list[0],input_images_list[1])
+        file_list = os.listdir('./')
+        print(file_list)
+        print ("Result is ", result_image_path)
+        return render(request, "main.html", {"images": images_slice, "result_image_path": result_image_path})
+    except:
+        return render(request, "main.html")
