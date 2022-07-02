bb2e681a931319638e7a7985314b02d84fcc9108
soiyo <soiyosauce@gmail.com>
2022-07-03 04:49:47 +0900

    MOD image/models.py  image 앱 views.py 작성하다가 내림차순 필요하여 ImageModel 부분 내에  timestamp 추가, image 필드 FileFiled로 변경 및 변수명 image -> images 로 변경 (images=models.FileField부분은 인풋 사진(2개) 저장되는 곳입니다.)

--- image/models.py
+++ image/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from user.models import User as UserModel
+from datetime import datetime
 # Create your models here.
 
 
 class ImageModel(models.Model):
     title = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(default=datetime.now())
     # like = models.ArrayField(null=False)
     # user = models.ForeignKey(UserModel, on_delete=models.CASCADE, null=False)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    images = models.FileField()
 # 제목으로 파일을 구분할 수 있게
-
     def __str__(self):
         return str(self.title)
