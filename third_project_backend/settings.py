320e351722f80e28e91aa6cfdcdca88350862257
soiyo <soiyosauce@gmail.com>
2022-07-03 04:48:10 +0900

    MOD settings.py os import, ‘image’ app 추가, TEMPLATES : ‘DIRS’  부분 수정, 맨 아래 MEDIA_ROOT & MEDIA_URL 경로 설정해둠 (인풋 파일 경로설정)

--- third_project_backend/settings.py
+++ third_project_backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,7 +88,7 @@ ROOT_URLCONF = 'third_project_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -191,3 +191,6 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
