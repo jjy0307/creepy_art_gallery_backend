714bedf11f6016066fc1b1bb99ee8aee274da284
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-03 21:16:10 +0900

    MOD settings.py
    update mysql

--- third_project_backend/settings.py
+++ third_project_backend/settings.py
@@ -107,13 +107,17 @@ WSGI_APPLICATION = 'third_project_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+# MySQL Database
+import custom_settings
+
+DATABASES = custom_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
