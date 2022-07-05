990cbea05567634d030d60e1a2d462bc9b10e126
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-06 06:03:20 +0900

    MOD views.py
     change views

--- image/views.py
+++ image/views.py
@@ -1,3 +1,6 @@
+from pydoc import Doc
+from django.http import JsonResponse
+from django.views import View
 from django.shortcuts import render
 from nst import change_image
 # Create your views here.
@@ -19,43 +22,70 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 import io
 import sys
 from base64 import b64encode
+import jwt
 
 
 @csrf_exempt
 @api_view(('POST',))
 def upload(request):
     if request.method == "POST":
+        # try:
+        # 1번 사진 / 기본
+        first_image = str(request.FILES['photos_0'])
+        first_image = "upload_image/" + first_image.replace(" ", "_")
+        # 2번 사진 / 명화
+        second_image = str(request.FILES['photos_1'])
+        second_image = "upload_image/" + second_image.replace(" ", "_")
+
+        # 1번 사진 / 기본
+        default_storage.save(f'{first_image}', ContentFile(
+            request.FILES['photos_0'].read()))
+        # 2번 사진 / 명화
+        default_storage.save(f'{second_image}', ContentFile(
+            request.FILES['photos_1'].read()))
+
+        ai_result_img, ai_result_name = change_image(
+            first_image, second_image)  # 여기에 first_image, second_image 넣어주세요
+
+        ai_result_img.convert('RGB')
+        ai_result_img.resize(ai_result_img.size, Image.ANTIALIAS)
+        output = io.BytesIO()
+        ai_result_img.save(output, format='JPEG', quality=100)
+        output.seek(0)
+        ImageModel.objects.create(
+            user=request.user,
+            images=InMemoryUploadedFile(
+                output, "ImageField", ai_result_name, 'image/jpeg', sys.getsizeof(output), None),
+            title=ai_result_name
+        )
+        ResultImageId = ImageModel.objects.get(title=ai_result_name).id
+        OtherImage = ImageModel.objects.all().exclude(user=request.user)
+        return Response({'message': "성공", "other_img": OtherImage, "image_id": ResultImageId, "data": b64encode(open("media/pics/" + ai_result_name, 'rb').read())})
+        # except:
+        #     return Response({'message': '실패'})
+
+
+@csrf_exempt
+@api_view(('PUT',))
+def get_image_info(request):
+    if request.method == 'PUT':
         try:
-            # 1번 사진 / 기본
-            first_image = str(request.FILES['photos_0'])
-            first_image = "upload_image/" + first_image.replace(" ", "_")
-            # 2번 사진 / 명화
-            second_image = str(request.FILES['photos_1'])
-            second_image = "upload_image/" + second_image.replace(" ", "_")
+            img_subscription = request.data['img_sub']
+            img_user = request.data['img_id']
+            ImageToChanage = ImageModel.objects.get(id=img_user)
+            ImageToChanage.subscription = img_subscription
+            return Response({'message': '성공'})
+        except:
+            return Response({'message': '실패'})
 
-            # 1번 사진 / 기본
-            default_storage.save(f'{first_image}', ContentFile(
-                request.FILES['photos_0'].read()))
-            # 2번 사진 / 명화
-            default_storage.save(f'{second_image}', ContentFile(
-                request.FILES['photos_1'].read()))
 
-            ai_result_img, ai_result_name = change_image(
-                first_image, second_image)  # 여기에 first_image, second_image 넣어주세요
-
-            ai_result_img.convert('RGB')
-            ai_result_img.resize(ai_result_img.size, Image.ANTIALIAS)
-            output = io.BytesIO()
-            ai_result_img.save(output, format='JPEG', quality=100)
-            output.seek(0)
-            ImageModel.objects.create(
-                images=InMemoryUploadedFile(
-                    output, "ImageField", ai_result_name, 'image/jpeg', sys.getsizeof(output), None),
-                title=ai_result_name
-            )
-            ResultImage = ImageModel.objects.get(title=ai_result_name)
-            data = ImageSerializer(ResultImage, data=request.data)
-
-            return Response({'message': "성공", "data": b64encode(open("media/pics/" + ai_result_name, 'rb').read())})
+@csrf_exempt
+@api_view(('GET',))
+def get_image(request, id):
+    if request.method == 'GET':
+        print(id)
+        try:
+            DownloadImage = ImageModel.objects.get(id=id).images
+            return Response({DownloadImage})
         except:
             return Response({'message': '실패'})
