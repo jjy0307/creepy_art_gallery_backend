d6b1e268387025377ea3a2ce6b1a9bd9b89cb857
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-05 15:05:02 +0900

    MOD image/views.py
    fetch로 데이터 받고 리턴하기 완료

--- image/views.py
+++ image/views.py
@@ -4,38 +4,54 @@ from nst import change_image
 from .models import ImageModel
 from django.views.decorators.csrf import csrf_exempt
 import os
+from user.models import User as UserModel
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from image.serializers import ImageSerializer
+from datetime import datetime
+
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.core.files.images import ImageFile
+
+from PIL import Image
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import io
+import sys
+from base64 import b64encode
 
 
 @csrf_exempt
+@api_view(('POST',))
 def upload(request):
     if request.method == "POST":
-        images = request.FILES.getlist("images")
-
-        for img in images:
-            ImageModel.objects.create(images=img)
-    images = ImageModel.objects.all()  # input 이미지 url 불러옴
-    images_slice = ImageModel.objects.all().order_by(
-        '-timestamp')[0:2]  # 가장 최근에 넣은 input 이미지 url
-    # print(images_slice)
-    input_images_list = []
-    for image_num in (0, 2):
         try:
-            images_values = ImageModel.objects.values(
-                'images').order_by('-timestamp')[image_num]['images']
-            input_images_list.append(images_values)
+            # 1번 사진 / 기본
+            first_image = str(request.FILES['photos_0'])
+            first_image = "upload_image/" + first_image.replace(" ", "_")
+            # 2번 사진 / 명화
+            # second_image = str(request.FILES['photos_0'])
+            # second_image = "upload_image/" + second_image.replace(" ", "_")
+
+            default_storage.save(f'{first_image}', ContentFile(
+                request.FILES['photos_0'].read()))
+
+            ai_result_img, ai_result_name = change_image(
+                first_image, first_image)  # 여기에 first_image, second_image 넣어주세요
+
+            ai_result_img.convert('RGB')
+            ai_result_img.resize(ai_result_img.size, Image.ANTIALIAS)
+            output = io.BytesIO()
+            ai_result_img.save(output, format='JPEG', quality=100)
+            output.seek(0)
+            ImageModel.objects.create(
+                images=InMemoryUploadedFile(
+                    output, "ImageField", ai_result_name, 'image/jpeg', sys.getsizeof(output), None),
+                title=ai_result_name
+            )
+            ResultImage = ImageModel.objects.get(title=ai_result_name)
+            data = ImageSerializer(ResultImage, data=request.data)
+
+            return Response({'message': "성공", "data": b64encode(open("media/pics/" + ai_result_name, 'rb').read())})
         except:
-            pass
-    print("input_images_list", input_images_list)
-    try:
-        result_image_path = change_image(
-            input_images_list[0], input_images_list[1])
-        result_image_path
-        # change_image(input_images_list[0],input_images_list[1])
-        file_list = os.listdir('./')
-        print("file_list", file_list)
-        print("Result is ", result_image_path)
-        return None
-        # return render(request, "main.html", {"images": images_slice, "result_image_path": result_image_path})
-    except:
-        return None
-        # return render(request, "main.html")
+            return Response({'message': '실패'})
