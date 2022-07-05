69f981b1fb2d05bf0c7cd3276806a9af1e4371b0
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-06 06:02:39 +0900

    MOD models.py
    chanag models

--- image/models.py
+++ image/models.py
@@ -6,10 +6,10 @@ from django.utils import timezone
 
 
 class ImageModel(models.Model):
-    # user = models.ForeignKey(UserModel, on_delete=models.CASCADE, default="")
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     title = models.CharField(max_length=256)
     timestamp = models.DateTimeField(default=datetime.now(tz=timezone.utc))
-    # like = models.ArrayField(null=False)
+    subscription = models.TextField(max_length=500, blank=True)
     images = models.FileField(upload_to='pics')
 # 제목으로 파일을 구분할 수 있게
 
