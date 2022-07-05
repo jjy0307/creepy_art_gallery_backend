ee2c482042aaaf3bd92c524566ba2abe8b825f22
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-06 06:03:10 +0900

    MOD urls.py
    chanage urls

--- image/urls.py
+++ image/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.upload, name="upload"),
-] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+    path("save/", views.get_image_info, name="getImageInfo"),
+    path("download/<id>", views.get_image_info, name="getImageInfo"),
+]
+
+# + static(
+#     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+# )
